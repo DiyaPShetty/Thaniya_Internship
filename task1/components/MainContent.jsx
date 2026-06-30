50922b3d6f5c85a453c01f7a5b0164c030a3292a
DiyaPShetty <shettydiya06@gmail.com>
2026-06-30 21:11:17 +0530

    Create MainContent.jsx

---
+++ task1/components/MainContent.jsx
@@ -0,0 +1,9 @@
+export default function MainContent() {
+  return (
+    <div className="main-content">
+      <h1>Dashboard</h1>
+
+      <p>Welcome to IssueFlow Dashboard.</p>
+    </div>
+  );
+}
