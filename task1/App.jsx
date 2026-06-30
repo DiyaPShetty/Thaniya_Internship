300b545fa8bf599eda671cb5e1fbd0ff8a3d44fd
DiyaPShetty <shettydiya06@gmail.com>
2026-06-30 21:04:47 +0530

    Update App.jsx

--- task1/App.jsx
+++ task1/App.jsx
@@ -1,1 +1,17 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
 
+export default function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+
+      <div className="content">
+        <Header />
+        <MainContent />
+      </div>
+    </div>
+  );
+}
