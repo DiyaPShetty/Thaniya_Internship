5285aef56d95bc95289e77d04954420fe150cabf
DiyaPShetty <shettydiya06@gmail.com>
2026-06-30 21:10:07 +0530

    Create Header.jsx

---
+++ task1/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h2>IssueFlow</h2>
+
+      <div className="profile">
+        🔍 👤
+      </div>
+    </div>
+  );
+}
