91caf211a5d4bcf228a9582349ea5b02852d5617
DiyaPShetty <shettydiya06@gmail.com>
2026-06-30 21:12:09 +0530

    Add Sidebar component with navigation links

---
+++ task1/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>IssueFlow</h2>
+
+      <ul>
+        <li>Dashboard</li>
+        <li>Issues</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+}
